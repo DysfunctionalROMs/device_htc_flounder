053970e274e53c502857f3359ce34717f09d7f0c
D-FUSE <dfuse06@gmail.com>
2016-02-21 01:49:30 -0600

    permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -136,3 +136,4 @@ KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/htc/flounder
 TARGET_KERNEL_CONFIG := flounder_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
