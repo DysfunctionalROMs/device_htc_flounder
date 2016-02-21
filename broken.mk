a861795095c4456489bd27123b371c352d927782
dfuse06 <dfuse06@gmail.com>
2016-02-21 01:41:22 -0600

    Broke It Bitches!

---
+++ broken.mk
@@ -0,0 +1,35 @@
+# Broken Os Specific Changes
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2048
+TARGET_SCREEN_WIDTH := 1536
+
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
+# Inherit some common broken stuff.
+$(call inherit-product, vendor/broken/config/common_full_tablet_wifionly.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/htc/flounder/aosp_flounder.mk)
+
+$(call inherit-product-if-exists, vendor/htc/flounder/device-vendor.mk)
+
+# Extra Packages
+PRODUCT_PACKAGES += \
+    com.android.nfc_extras
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=flounder \
+    BUILD_FINGERPRINT=google/volantis/flounder:6.0.1/MMB29R/2482564:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 6.0.1 MMB29R 2482564 release-keys" \
+    BUILD_ID=MMB29R
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := broken_flounder
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 9
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,4 +14,4 @@
 # limitations under the License.
 #
 
-add_lunch_combo aosp_flounder-userdebug
+add_lunch_combo broken_flounder-userdebug
